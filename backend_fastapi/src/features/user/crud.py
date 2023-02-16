2a11b1e0a5a496511935e2f566c512bddcf59150
mohammadammash <mohammadammash31@gmail.com>
2023-02-16 17:49:10 +0200

    [Fix] fix model import

--- backend_fastapi/src/features/user/crud.py
+++ backend_fastapi/src/features/user/crud.py
@@ -1,7 +1,7 @@
 from typing import List
 # internal:
 from ...config.database import UsersCollection
-from ...models import UserModel
+from ...models.user import UserModel
 
 async def get_all_users() -> List[UserModel]:
     users = await UsersCollection.find().to_list(1000)
