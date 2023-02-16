4d5e94b5d13a44aa70b4298652eb69cb722619b9
mohammadammash <mohammadammash31@gmail.com>
2023-02-16 14:17:27 +0200

    [Create]: create .env config folder to hold database and setting senv variables

---
+++ backend_fastapi/src/config/main.py
@@ -0,0 +1,17 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    MONGODB_URI: str
+    MONGODB_PORT: str
+    DB_NAME: str
+    CLIENT_PORT: str
+    SERVER_PORT: str
+    SERVER_ENV: str
+    JWT_SECRET_KEY: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
--- backend_fastapi/src/main.py
+++ backend_fastapi/src/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
-
+#internal:
+from .config.main import settings
 
 app = FastAPI()
 
-origins = [os.getenv("CLIENT_PORT")]
+origins = [settings.CLIENT_PORT]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
