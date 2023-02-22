59270471599b2df3a42b6254a91c9db5d89dfaf9
mohammadammash <mohammadammash31@gmail.com>
2023-02-22 17:32:56 +0200

    [Add]: NewMessageSchema

--- backend_fastapi/src/models/message.py
+++ backend_fastapi/src/models/message.py
@@ -1,14 +1,46 @@
 from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel, Field
+from bson import ObjectId
+# internal
+from ..config.mongodb_objectid import PyObjectId
 
 class MessageModel(BaseModel):
-    _id: str
-    created_at: datetime = Field(default_factory=datetime.now)
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    created_at: Optional[datetime] = Field(default_factory=datetime.now)
     text: str = Field(..., min_length=1)
     is_author: bool
     image_url: Optional[str] = None
     voice_url: Optional[str] = None
     
     class Config:
-        orm_mode = True
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "created_at": "2023-02-22",
+                "text": "Hello Babe",
+                "is_author": True,
+                "image_url": "db/public/images/user1.png",
+                "voice_url": "db/public/voices/user1.mp3"
+            }
+        }
+        
+class NewMessageModel(BaseModel):
+    text: str = Field(..., min_length=1)
+    is_author: bool
+    image_base64: Optional[str] = None
+    voice_data: Optional[str] = None
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "text": "Hello Honey!!",
+                "is_author": False,
+                "image_base64": "FGIDSGIKSDIF23W",
+                "voice_data": "THISISMYVOICEDATA"
+            }
+        }
