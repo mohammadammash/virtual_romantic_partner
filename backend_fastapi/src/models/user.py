ab897920fb181364b69587c781a5bb95d5bc2198
mohammadammash <mohammadammash31@gmail.com>
2023-02-22 16:46:50 +0200

    [Add]: SignUp New User Schema

--- backend_fastapi/src/models/user.py
+++ backend_fastapi/src/models/user.py
@@ -1,11 +1,13 @@
 from datetime import date
 from typing import Optional, List
 from pydantic import BaseModel, Field
+from bson import ObjectId
 # internal:
 from .message import MessageModel
 
+
 class UserModel(BaseModel):
-    _id: str
+    _id: ObjectId
     name: str = Field(..., min_length=1)
     email: str = Field(..., regex=r'^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$')
     password: str = Field(..., min_length=6)
@@ -17,3 +19,14 @@ class UserModel(BaseModel):
 
     class Config:
         orm_mode = True
+
+class NewUserModel(BaseModel):
+    name: str = Field(..., min_length=1)
+    email: str = Field(..., regex=r'^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$')
+    password: str = Field(..., min_length=6)
+    gender: str = Field(..., enum=['male', 'female', 'other'])
+    partner_gender: str = Field(..., enum=['male', 'female', 'other'])
+    dob: date
+    profile_base64: Optional[str] = None
+
+
