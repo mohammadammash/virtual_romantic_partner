6f2ff74163bc2ce50da063e113fce211691fd4d2
mohammadammash <mohammadammash31@gmail.com>
2023-02-22 16:48:20 +0200

    [Add]: SignUp User Create function

--- backend_fastapi/src/features/user/crud.py
+++ backend_fastapi/src/features/user/crud.py
@@ -1,8 +1,22 @@
 from typing import List
+from fastapi import status, HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 # internal:
 from ...config.database import UsersCollection
-from ...models.user import UserModel
+from ...models.user import UserModel, NewUserModel
 
 async def get_all_users() -> List[UserModel]:
     users = await UsersCollection.find().to_list(1000)
     return users
+
+
+
+async def post_signup_user(data: NewUserModel) -> UserModel:
+    user = jsonable_encoder(data)
+    new_user = await UsersCollection.insert_one(user)
+    created_user = await UsersCollection.find_one({"_id": new_user.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_user)
+
+
+
