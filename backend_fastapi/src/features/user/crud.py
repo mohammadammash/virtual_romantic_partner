afec3ef61477a610640d47f287e0247cf204ecbc
mohammadammash <mohammadammash31@gmail.com>
2023-02-22 17:19:07 +0200

    [Add]: GetPaginatedMessages basic route and controller

--- backend_fastapi/src/features/user/crud.py
+++ backend_fastapi/src/features/user/crud.py
@@ -1,10 +1,9 @@
 from typing import List
-from fastapi import status, HTTPException
 from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
 # internal:
 from ...config.database import UsersCollection
 from ...models.user import UserModel, NewUserModel
+from ...models.message import MessageModel
 
 async def get_all_users() -> List[UserModel]:
     users = await UsersCollection.find().to_list(1000)
@@ -18,4 +17,5 @@ async def post_signup_user(data: NewUserModel) -> UserModel:
     return created_user
 
 
-
+async def get_paginated_messages(offset: int) -> List[MessageModel]:
+    return []
--- backend_fastapi/src/features/user/main.py
+++ backend_fastapi/src/features/user/main.py
@@ -3,6 +3,7 @@ from typing import List
 # internal:
 from . import crud
 from ...models.user import NewUserModel, UserModel
+from ...models.message import MessageModel
 
 router = APIRouter(
     prefix="/user",
@@ -12,6 +13,10 @@ router = APIRouter(
 @router.get("/", response_description="All Users", response_model=List[UserModel])
 async def get_all_users():
     return await crud.get_all_users()
+
+@router.get("/chat/{offset}", response_description="Get Paginated 10 Messages", response_model=List[MessageModel])
+async def get_messages(offset: int):
+    return await crud.get_paginated_messages(offset=offset)
 
 @router.post("/", response_description="User Sign Up", response_model=UserModel)
 async def signup_user(data: NewUserModel):
