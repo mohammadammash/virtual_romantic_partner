ee1ad0dcb69e658f29ab21ee57885be29b3c7755
mohammadammash <mohammadammash31@gmail.com>
2023-02-22 16:47:39 +0200

    [Add]: SignUp New User Route

--- backend_fastapi/src/features/user/main.py
+++ backend_fastapi/src/features/user/main.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Request
 from typing import List
 # internal:
 from . import crud
-from ...models.user import UserModel
+from ...models.user import NewUserModel, UserModel
 
 router = APIRouter(
     prefix="/user",
@@ -12,3 +12,9 @@ router = APIRouter(
 @router.get("/", response_description="All Users", response_model=List[UserModel])
 async def get_all_users():
     return await crud.get_all_users()
+
+@router.post("/", response_description="User Sign Up", response_model=UserModel)
+async def signup_user(data: NewUserModel):
+    return await crud.post_signup_user(data=data)
+
+
