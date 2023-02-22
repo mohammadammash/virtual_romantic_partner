5d3fd513857086f33d4745f9c9628ba64d16d125
mohammadammash <mohammadammash31@gmail.com>
2023-02-22 17:09:35 +0200

    [Update]: Add PyObjectId and json serializers

--- backend_fastapi/src/models/user.py
+++ backend_fastapi/src/models/user.py
@@ -4,21 +4,34 @@ from pydantic import BaseModel, Field
 from bson import ObjectId
 # internal:
 from .message import MessageModel
+from ..config.mongodb_objectid import PyObjectId
 
 
 class UserModel(BaseModel):
-    _id: ObjectId
-    name: str = Field(..., min_length=1)
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
     email: str = Field(..., regex=r'^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$')
     password: str = Field(..., min_length=6)
     gender: str = Field(..., enum=['male', 'female', 'other'])
     partner_gender: str = Field(..., enum=['male', 'female', 'other'])
     dob: date
     profile_url: Optional[str] = None
-    chat: List[MessageModel]
-
+    chat: Optional[List[MessageModel]] = []
     class Config:
-        orm_mode = True
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Jane Doe",
+                "email": "jdoe@example.com",
+                "gender": "male",
+                "partner_gender": "female",
+                "dob": "2023-02-22",
+                "profile_url": "/public/images/ahmad@gmail.com24532432",
+                "chat": "[]",
+            }
+        }
 
 class NewUserModel(BaseModel):
     name: str = Field(..., min_length=1)
@@ -28,5 +41,20 @@ class NewUserModel(BaseModel):
     partner_gender: str = Field(..., enum=['male', 'female', 'other'])
     dob: date
     profile_base64: Optional[str] = None
+    
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Jane Doe",
+                "email": "jdoe@example.com",
+                "password": "123PleaseDontUseMe",
+                "gender": "female",
+                "partner_gender": "male",
+                "dob": "2023-02-18",
+                "profile_base64": "GSFDKGSID#$#@#LFDSFD",
+            }
+        }
 
 
