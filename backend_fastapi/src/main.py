c6194135827baed7a2b96768831754bcb962eeab
mohammadammash <mohammadammash31@gmail.com>
2023-03-02 11:04:44 +0200

    [Add]: Common Routes along with Basic Controller

--- backend_fastapi/src/main.py
+++ backend_fastapi/src/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os
 #internal:
 from .config.main import settings
 from .features.user.main import router as user_router
 from .features.admin.main import router as admin_router
+from .features.common.main import router as common_router
 
 app = FastAPI()
 
@@ -19,3 +19,4 @@ app.add_middleware(
 
 app.include_router(user_router)
 app.include_router(admin_router)
+app.include_router(common_router)
