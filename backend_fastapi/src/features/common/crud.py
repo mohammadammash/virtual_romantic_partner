3d0e7548a2a23c6d369d602de81f9117d70413a6
mohammadammash <mohammadammash31@gmail.com>
2023-03-03 11:25:55 +0200

    [Add]: LoginUserModel retrieve on login and retrieve user if matches

--- backend_fastapi/src/features/common/crud.py
+++ backend_fastapi/src/features/common/crud.py
@@ -1,11 +1,21 @@
 from bson import ObjectId
+from fastapi import HTTPException
+from passlib.context import CryptContext
 # internal:
 from ...config.database import UsersCollection
-from ...models.user import UserModel
+from ...models.user import UserModel, LoginUserModel
 
-async def post_login_user(user_id: str) -> UserModel:
-    user = await UsersCollection.find_one({"_id": ObjectId(user_id)})
-    return user
+async def post_login_user(data: LoginUserModel) -> UserModel:
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    
+    user = await UsersCollection.find_one({"email": data.email})
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    if not pwd_context.verify(data.password, user["password"]):
+        raise HTTPException(status_code=401, detail="Incorrect email or password")
+    
+    return UserModel(**user)
 
 async def put_edit_profile() -> UserModel:
     user = await UsersCollection.find_one({"_id": ObjectId("64005e70cf5c276361cf0ee1")})
