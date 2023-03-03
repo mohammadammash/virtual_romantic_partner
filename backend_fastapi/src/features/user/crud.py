2d0fb339e092b9fdd00e9d02cda20e0e21449577
mohammadammash <mohammadammash31@gmail.com>
2023-03-03 10:51:18 +0200

    [Add]: post_send_message crud logic

--- backend_fastapi/src/features/user/crud.py
+++ backend_fastapi/src/features/user/crud.py
@@ -1,13 +1,14 @@
 from typing import List
 from fastapi import HTTPException
+from bson import ObjectId
 from fastapi.encoders import jsonable_encoder
 # internal:
 from ...config.database import UsersCollection
 from ...models.user import UserModel, NewUserModel
 from ...models.message import MessageModel, NewMessageModel
 
+
 async def post_signup_user(data: NewUserModel) -> UserModel:
-    # Check if user with this email already exists
     existing_user = await UsersCollection.find_one({"email": data.email})
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already exists")
@@ -19,11 +20,36 @@ async def post_signup_user(data: NewUserModel) -> UserModel:
 
 
 async def get_paginated_messages(user_id: str, offset: int) -> List[MessageModel]:
-    return []
+    user = await UsersCollection.find_one({"_id": user_id})
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    messages = user.get("chats", [])
+    start = offset
+    end = start + 10
+    paginated_messages = [MessageModel(**message) for message in messages[start:end]]
+    return paginated_messages
 
 
-async def post_send_message(data: NewMessageModel) -> MessageModel:
+async def post_send_message(data: NewMessageModel)->MessageModel:
+    user = await UsersCollection.find_one({"_id": ObjectId("64005e70cf5c276361cf0ee1")})
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
     message = jsonable_encoder(data)
-    new_message = await UsersCollection.insert_one(message)
-    sent_message = await UsersCollection.find_one({"_id": new_message.inserted_id})
-    return sent_message
+    message_id = ObjectId()
+    message.update({"_id": str(message_id)})
+    # prepare response message
+    response_message = MessageModel(
+        id=str(ObjectId()),
+        is_author=False,
+        text="hey_dude",
+    )
+    
+    # push both message once to the db
+    result = await UsersCollection.update_one({"_id": user["_id"]}, {"$push": {"chats": {"$each": [message, jsonable_encoder(response_message)]}}})
+    
+    if not result.modified_count:
+        raise HTTPException(status_code=500, detail="Failed to send message")
+    
+    return response_message
