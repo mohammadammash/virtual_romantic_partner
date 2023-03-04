2d087ddea7027c4e2183280c73de875e25138888
mohammadammash <mohammadammash31@gmail.com>
2023-03-04 14:07:01 +0200

    [Add]: JWT package and include auth dependency to common routes

--- backend_fastapi/src/features/auth/main.py
+++ backend_fastapi/src/features/auth/main.py
@@ -8,10 +8,10 @@ router = APIRouter(
     prefix="/auth"
 )
 
-@router.post("/login", response_description="Login User", response_model=UserModel)
+@router.post("/login", response_description="Login User")
 async def post_login_user(data: LoginUserModel = Body(...)):
     return await crud.post_login_user(data=data)
 
-@router.post("/signup", response_description="User Sign Up", response_model=UserModel)
+@router.post("/signup", response_description="User Sign Up")
 async def signup_user(data: NewUserModel):
     return await crud.post_signup_user(data=data)
